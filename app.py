3f2dbeae1d481601a8bdfec28ee30ba0361e1f72
Teja-red-E <98870150+Teja-red-E@users.noreply.github.com>
2024-06-13 20:04:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import av
+import logging
 
 class VideoTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.logger = logging.getLogger("VideoTransformer")
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        self.logger.addHandler(handler)
+
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
-        # Just return the frame without any transformation for testing
+        self.logger.debug("Frame received: shape=%s", img.shape)
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 st.title("Webcam Access Test")
