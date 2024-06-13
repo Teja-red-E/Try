e46ef1be3bdd51ca6977fd17af1782e0b0bddc4d
Teja-red-E <98870150+Teja-red-E@users.noreply.github.com>
2024-06-13 20:23:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-def main():
-    st.title("Minimal Webcam Access Test")
-
-    webrtc_ctx = webrtc_streamer(key="example")
-
-    if webrtc_ctx.state.playing:
-        st.write("Webcam is connected!")
-    else:
-        st.write("Error connecting to webcam.")
-
-if __name__ == "__main__":
-    main()
+webrtc_streamer(key="example")
